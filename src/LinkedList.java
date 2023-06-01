81a0d44dd3594eb7e07d03af2eb4145c4d2ac485
aoi-cdev <matthewcheung1023@gmail.com>
2023-06-01 16:41:41 -0600

    Added a doubly linked list class Added a working doubly linked list class with a built in inner class that implements Iterator<E>. The linked list class is only able to iterate through the array with next() and previous() method as well because that's all I need for the situation it's going to be used in. The class should be working but tests will be conducted in the future before further progression in the project. (unit test source code will be included in a seperate folder under the root directory)

---
+++ src/LinkedList.java
@@ -0,0 +1,119 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedList<E> {
+
+	private Node<E> head;
+	private Node<E> tail;
+	private Node<E> currentNode;
+	
+	private class LinkedListIterator implements Iterator<E> {				// The way to declare an inner class which implements an interface is provided by chatgpt.
+																			// url: https://chat.openai.com/share/463e57e7-331e-46dc-b4d6-2a99d5923f66
+		private Node<E> currentNode = null;
+
+		public boolean hasNext() {
+			return (this.currentNode.getNext() != null) ? true : false;
+		}
+
+		public E next() {
+			if (this.currentNode == null) {
+				this.currentNode = LinkedList.this.head;
+				return this.currentNode.getContent();
+			}
+			this.currentNode = this.currentNode.getNext();
+			return this.currentNode.getContent();
+		}
+
+	}
+
+	public LinkedList(Node<E> initialNode) {
+		this.head = initialNode;
+		this.tail = initialNode;
+		this.currentNode = initialNode;
+	}
+
+	public Iterator<E> getIterator() {
+		return new LinkedListIterator();
+	}
+
+	public boolean hasNext() {
+		return (currentNode.getNext() != null) ? true : false;
+	}
+
+	public boolean hasPrevious() {
+		return (currentNode.getPrevious() != null) ? true : false;
+	}
+
+	public void next() {
+		if (!hasNext()) {
+			throw new NoSuchElementException("next node does not exist");
+		}
+
+		currentNode = currentNode.getNext();
+
+		if (!hasNext()) {
+			this.tail = currentNode;
+		}
+	}
+
+	public void previous() {
+		if (!hasPrevious()) {
+			throw new NoSuchElementException("previous node does not exist");
+		}
+
+		currentNode = currentNode.getPrevious();
+
+		if (!hasPrevious()) {
+			this.head = currentNode;
+		}
+	}
+
+	public Node<E> getHead() {
+		return this.head;
+	}
+
+	public Node<E> getTail() {
+		return this.tail;
+	}
+
+	public E getContent() {
+		return currentNode.getContent();
+	}
+
+	public void set(E content) {
+		currentNode.setContent(content);
+	}
+
+	public void add(E content) {
+		Node<E> newNode = new Node<E>(currentNode.getPrevious(), 
+									  content, 
+									  currentNode.getNext());
+		
+		currentNode.setNext(newNode);
+
+		Node<E> nextNode = newNode.getNext();
+
+		if (nextNode == null) {
+			this.tail = newNode;
+			return;
+		}
+
+		newNode.getNext()
+			   .setPrevious(newNode);
+	}
+
+	public E[] getList() {
+		Iterator<E> iterator = getIterator();
+		ArrayList<E> output = new ArrayList<E>();
+
+		while(iterator.hasNext()) {
+			output.add(iterator.next());
+		}
+
+		return (E[]) output.toArray();		// Doesn't have a better solution due to language problem as far as I know.
+											// The ArrayList should only ever be able to contain elements of type E so this should be safe.
+	}
+}
---
+++ src/Node.java
@@ -0,0 +1,54 @@
+package src;
+
+public class Node<E> {
+    
+    private Node<E> previous;
+    private Node<E> next;
+    private E content;
+
+    public Node(E content) {
+        this(null, content, null);
+    }
+
+    public Node(Node<E> previous, E content) {
+        this(previous, content, null);
+    }
+
+    public Node(E content, Node<E> next) {
+        this(null, content, next);
+    }
+
+    public Node(Node<E> previous, E content, Node<E> next) {
+        this.previous = previous;
+        this.next = next;
+        this.content = content;
+    }
+
+    public Node<E> getNext() {
+        return this.next;
+    }
+
+    public Node<E> getPrevious() {
+        return this.previous;
+    }
+
+    public E getContent() {
+        return this.content;
+    }
+
+    public void setNext(Node<E> newNext) {
+        this.next = newNext;
+    }
+
+    public void setPrevious(Node<E> newPrevious) {
+        this.previous = newPrevious;
+    }
+
+    public void setContent(E newContent) {
+        this.content = newContent;
+    }
+
+    public Node<E> clone() {
+        return new Node<E>(this.previous, this.content, this.next);
+    }
+}
